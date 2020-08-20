8dd70380f20bcef00052fd07f6b7c3c4b5ace28d
Mike Champagne <geauxdrum@gmail.com>
2020-08-20 10:38:26 -0500

    changes pointers to bundle.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   apps: [{
-    name: 'champagne',
+    name: 'champagne_proxy',
     script: './server/server.js'
   }],
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-3-17-12-10.us-east-2.compute.amazonaws.com',
+      host: 'ec2-52-15-237-49.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/front-end-capstone.pem',
       ref: 'origin/master',
       repo: 'git@github.com:ATX-50-Team-Best-Buy/Champagne_proxy.git',
-      path: '/home/ubuntu/champagne',
+      path: '/home/ubuntu/champagne_proxy',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const path = require('path')
-const port = 2003;
+const port = 5002;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, '/../public/')))
